8c29afce539928a9943c39b96f11a5b0744aba23
Andrew Tsarow <andrew.tsarow@gmail.com>
2023-12-31 21:08:58 +0100

    v1.1.0

--- src/use-server-action.ts
+++ src/use-server-action.ts
@@ -2,26 +2,26 @@ import { useState } from "react"
 
 import { serializeError } from "./serialize-error"
 
-export function useServerAction<T extends (...args: any) => Promise<any>>(action: T) {
-  const [data, setData] = useState<Awaited<ReturnType<T>> | null | undefined>(null)
-  const [error, setError] = useState<Error | null | undefined>(null)
+export function useServerAction<T extends (...args: any) => any>(action: T) {
+  const [data, setData] = useState<Awaited<ReturnType<T>>>()
+  const [error, setError] = useState<Error>()
   const [loading, setLoading] = useState(false)
 
-  async function execute(...args: Parameters<T>) {
-    setData(null)
-    setError(null)
+  async function execute(...args: Parameters<T>): Promise<Awaited<ReturnType<T>>> {
+    setData(undefined)
+    setError(undefined)
     setLoading(true)
 
     try {
       const response = await action(...args)
       setData(response)
+      return data
     } catch (err) {
       setError(serializeError(err as Error))
+      throw err
     } finally {
       setLoading(false)
     }
-
-    return data
   }
 
   return { data, error, execute, loading }
